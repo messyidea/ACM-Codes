c3661af310c5833610ff979cadc65f4404c4c53b
messyidea <the.mid.sea@gmail.com>
2015-09-19 23:11:49 +0800

    modify baidu

--- 比赛/baidu/c.cpp
+++ 比赛/baidu/c.cpp
@@ -32,46 +32,47 @@ const double pi=acos (-1.0);
 typedef long long ll;
 
 using namespace std;
-int dp[200005][4];
+int dp[200005][16];
 int ga[200005];
 int n, m;
-int a, b, c;
 int tt;
 int aa[2],bb[2];
-int toget(int a,int b,int c) {
+inline int toget(int a,int b,int c) {
     aa[0] = a / 4;
     aa[1] = a % 4;
     bb[0] = b / 4;
     bb[1] = b % 4;
+    int count = 0;
     if((abs(aa[0] - bb[0]) <= 1 && abs(aa[1] - bb[1]) <= 1) || (abs(aa[0] - bb[1]) <= 1 && abs(aa[1] - bb[0]) <= 1)) {
-        int count = 0;
-        if(c & (1 << bb[0])) count ++;
-        if(c & (1 << bb[1])) count ++;
-        if(bb[0] == bb[1]) count --;
+        count = 0;
+        if((c & (1 << bb[0])) > 0) count ++;
+        if((c & (1 << bb[1])) > 0) count ++;
+        if(bb[0] == bb[1] && count > 0) count --;
         return count;
-    } else return 0;
+    } else return count;
 }
 int main() {
-	freopen("input.txt","r",stdin); 
+	//freopen("input.txt","r",stdin); 
 	//freopen("out.txt","w",stdout); 
     while(~scanf("%d", &n)) {
         tt = 0;
         memset(ga, 0, sizeof(ga));
+        int a, b;
         rep(i,n) {
             scanf("%d %d",&a,&b);
             if(a >= tt) tt = a;
-            ga[a] |= 1 << b;
+            ga[a] |= 1 << (b-1);
         }
         for(int i=0;i<16;++i) {
             dp[0][i] = 0;
         }
-        for(int i=1; i <= tt; ++i) {
-            for(int j=0;j < 16;++j) {
-                dp[i][j] = 0;
-                for(int k=0;k < 16; ++k) {
-                    int ppp = dp[i-1][k];
-                    int qqq = toget(k,j,ga[i]);
-                    if(ppp + qqq > dp[i][j]) dp[i][j] = ppp + qqq;
+        for(int ii=1; ii <= tt; ++ii) {
+            for(int jj=0;jj < 16;++jj) {
+                dp[ii][jj] = 0;
+                for(int kk=0;kk < 16; ++kk) {
+                    int ppp = dp[ii-1][kk];
+                    int qqq = toget(kk,jj,ga[ii]);
+                    if(ppp + qqq > dp[ii][jj]) dp[ii][jj] = ppp + qqq;
                 }
             }
         }
